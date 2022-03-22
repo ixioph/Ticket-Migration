3ab5c70a51a7784500d71fbb848a8386dc4c81ff
Andrei Vorobcalo <90719230+AndreiVorobcalo@users.noreply.github.com>
2022-03-22 19:38:17 +0200

    Delete test.py

--- Ticket-Migration-main/src/test.py
+++
@@ -1,8 +0,0 @@
-from encodings import utf_8
-import json
-
-data = []
-with open("/Users/avorobcalo/Downloads/mystuff/Python/Ticket-Migration-main/src/test.json") as jsonFile:
-    s = jsonFile.read()
-    data = json.loads(s)
-print(data)
